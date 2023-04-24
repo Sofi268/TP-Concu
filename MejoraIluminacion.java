ab569a2f7f0549e5894fb63cb4696850466fd3bc
FedeArnaudo <131111577+FedeArnaudo@users.noreply.github.com>
2023-04-24 17:24:30 -0300

    Update MejoraIluminacion.java

--- MejoraIluminacion.java
+++ MejoraIluminacion.java
@@ -1,17 +1,10 @@
-import java.util.Random;
-
 /**
  * Proceso 2: Mejorar la iluminacion de cada imagen, en un tiempo aleatorio. cada imagen debe ser mejorada por los 3 hilos.
  * cantidad de hilos: 3
  */
-public class MejoraIluminacion implements Runnable{
-    private ContenedorInicial c;
-    private Random rand;
-    private boolean listo;
+public class MejoraIluminacion extends Edicion implements Runnable{
     public MejoraIluminacion(ContenedorInicial c) {
-        this.c = c;
-        rand = new Random();
-        listo = false;
+        super(c);
     }
 
     @Override
@@ -23,12 +16,6 @@ public class MejoraIluminacion implements Runnable{
 
     public void mejorarIluminacion(){ //syncronized
         c.getImagenAMejorar(rand.nextInt(100));
-        setListo();
-    }
-
-    public void setListo(){
-        if(c.getImagenesMejoradas() == 100){
-            listo = true;
-        }
+        setListo(c.getImagenesMejoradas());
     }
 }
