a610b6f6b93f01184f06446ce667b3bf817daaf9
Sofia Castro <116833245+Sofi268@users.noreply.github.com>
2023-04-25 11:28:00 -0300

    Update MejoraIluminacion.java

--- MejoraIluminacion.java
+++ MejoraIluminacion.java
@@ -1,6 +1,6 @@
 /**
  * Proceso 2: Mejorar la iluminacion de cada imagen, en un tiempo aleatorio. cada imagen debe ser mejorada por los 3 hilos.
- * cantidad de hilos:3
+ * cantidad de hilos: 3
  */
 public class MejoraIluminacion extends Edicion implements Runnable{
     public MejoraIluminacion(ContenedorInicial c) {
@@ -15,7 +15,7 @@ public class MejoraIluminacion extends Edicion implements Runnable{
     }
 
     public void mejorarIluminacion(){ //syncronized
-        c.getImagenAMejorar(rand.nextInt(100));
+        c.getImagenAMejorar();
         setListo(c.getImagenesMejoradas());
     }
 }
