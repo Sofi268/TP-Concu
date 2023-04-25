d86cb14630ca2461281f182cb6f2701075f4cd09
Sofia Castro <116833245+Sofi268@users.noreply.github.com>
2023-04-25 11:20:28 -0300

    Update CargarCopia.java

--- CargarCopia.java
+++ CargarCopia.java
@@ -6,7 +6,7 @@ public class CargarCopia implements Runnable {
     private ContenedorInicial ci;
     private ContenedorFinal cf;
     private boolean listo;
-    public CargarCopia(ContenedorInicial ci, ContenedorFinal cf, Thread[] hilos) {
+    public CargarCopia(ContenedorInicial ci, ContenedorFinal cf) {
         this.ci = ci;
         this.cf = cf;
         listo = false;
@@ -14,12 +14,10 @@ public class CargarCopia implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("inicio de copiado............");
         while(!listo){
-            cf.Copiar(ci);
+            ci.copiar(cf);
             if(cf.getImagenesCopiadas() == 100){
                 listo = true;
-                System.out.printf("%s : copiado completado.\n",Thread.currentThread().getName());
             }
         }
     }
