7432acfab9da4dd9a3a526e7beb02628c0305432
Sofia Castro <116833245+Sofi268@users.noreply.github.com>
2023-04-25 11:21:19 -0300

    Update ContenedorFinal.java

--- ContenedorFinal.java
+++ ContenedorFinal.java
@@ -9,28 +9,27 @@ public class ContenedorFinal {
     private ArrayList<Imagen> contenedorFinal;
     private Random rand;
     private double tiempo;
+    private int imagenesCopiadas;
     public ContenedorFinal(double tiempo) {
         contenedorFinal = new ArrayList<Imagen>(100);
         rand = new Random();
         this.tiempo = tiempo;
+        imagenesCopiadas = 0;
     }
-    public synchronized void Copiar(ContenedorInicial ci){
-        Imagen auxIm = ci.sacarImagen(rand.nextInt(100));//eliminar elemento del contenedor inicial.
-        if(auxIm != null){
-            String aux = Thread.currentThread().getName();
-            auxIm.setImprovements(3, aux);
-            auxIm.setNombre(auxIm.getNombre()+".copia");
-            contenedorFinal.add(auxIm);
-            try {
-                TimeUnit.MILLISECONDS.sleep((long)(tiempo/2)); //tiempo en copiar
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.printf("imagen copiada: %s\n",auxIm.getNombre());
+    public synchronized void pegarImagen(Imagen auxIm){
+        auxIm.setImprovements(3,  Thread.currentThread().getName());
+        auxIm.setNombre(auxIm.getNombre()+".copia");
+        contenedorFinal.add(auxIm);
+        imagenesCopiadas++;
+        try {
+            TimeUnit.MILLISECONDS.sleep((long)(tiempo/2)); //tiempo en copiar
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+        System.out.printf("imagen copiada: %s\n",auxIm.getNombre());
     }
     public synchronized int getImagenesCopiadas() {
-        return contenedorFinal.size();
+        return imagenesCopiadas;
     }
     //Proceso 4:
     public synchronized ArrayList<Imagen> getContenedorFinal(){
