d5ea9a2ff0f96ec1cdd4988220928e7d5a0714dc
MVene <127953128+MVene@users.noreply.github.com>
2023-04-27 21:25:01 -0300

    Update Ajustar.java

--- Ajustar.java
+++ Ajustar.java
@@ -1,12 +1,13 @@
 /**
  * Ajustar: Proceso encargado de ajustar el tamaño de la imagen de manera sincronizada
  * hilos que ejecutan: hilo 6/7/8
- * Acciones que realiza: *completar.
+ * Acciones que realiza: -agarro una imagen random y pregunto si listo es false y si esa imagen random fue ajustada de lo contrario la ajusto.
  */
 public class Ajustar implements Runnable{
     private long tiempo;
     private boolean listo;
     private int imagenesAjustadas;
+    protected ContenedorInicial c;
 
     public Ajustar(long tiempo) {
         this.tiempo = tiempo;
@@ -16,6 +17,29 @@ public class Ajustar implements Runnable{
 
     @Override
     public void run() {
-
+        Random rand = new Random();
+        int indiceImagenaleatoria = rand.nextInt(c.lenght);// de forma aleatoria elijo un indice
+        String imagenAleatoria = c[indiceImagenaleatoria];  // busco la imagen en ese indice 
+        
+             while(listo == false && //que esa imagen no este ajustada ){
+                  ajustar(5,2); 
+                 
+             }
+    }
+    
+    
+    public void setListo(int i){
+        if(i == 100){
+            listo = true;
+        }
+    }
+    
+    public void ajustar(int l, int a)
+    {
+         imagenesAjustadas ++;
+         System.out.println("Imagen Ajustada: " + s); //dudas
+         c.ajuste(l,a); // esta en contenedor inicial no se si se implementa ahi o que
+         
+         setListo(imagenesAjustadas); //me fijo si son 100 o que
     }
 }
