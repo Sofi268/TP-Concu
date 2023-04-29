24be326812d4de6bdaea534223a5b72898eccc15
Efrain Veliz <128051011+efrainveliz2023@users.noreply.github.com>
2023-04-29 14:47:41 -0300

    TesteoCargar_LISTO

--- Testeo.java
+++ Testeo.java
@@ -1,5 +1,7 @@
-import java.util.Date;
-
+/**
+ * CLase testeo se encarga de hacer pruebas unitarias a cada proceso del sistema.
+ * para verificar que los procesos se efectuan correctamente. y en el tiempo establecido.
+ */
 public class Testeo {
     private long tiempo;
     /**
@@ -8,11 +10,20 @@ public class Testeo {
      */
     public Testeo(long tiempo) {
         this.tiempo = tiempo;
-        System.out.println("---Inicio del programa---");
+        System.out.println("------------------------------------");
+        System.out.println("---Inicio del Testeo del programa---");
     }
+
+    /**
+     * Testeo del proceso 1, de cargar imagenes a un contenedor incial.
+     */
     public void test_Cargar(){
-        Date d1 = new Date();
-        Cargar c = new Cargar(tiempo);
+        boolean paso = true;
+        long startTime = System.currentTimeMillis();
+        System.out.printf("Test_carga: \n");
+
+        ContenedorInicial ci = new ContenedorInicial();
+        Cargar c = new Cargar(tiempo,ci);
         Thread hiloA = new Thread(c);
         Thread hiloB = new Thread(c);
         hiloA.start();
@@ -22,13 +33,53 @@ public class Testeo {
             hiloB.join();
         } catch (InterruptedException e) { throw new RuntimeException(e); }
 
-        Date d2 = new Date();
-        int t = 0;
-        if(d2.getSeconds() > d1.getSeconds()){
-            t = (d2.getMinutes()-d1.getMinutes()) + (d2.getSeconds()-d1.getSeconds());
-        }else{
-            t = (d2.getMinutes() - d1.getMinutes()) + ((d2.getSeconds()+60) - d1.getSeconds());
+        long endTime = System.currentTimeMillis();
+        long t = endTime - startTime; //tiempo en miliSegundos.
+
+        if( (t >= tiempo) && ( t <= (tiempo+1000)) ){ System.out.printf("|->Tiempo: (%dms) - True\n",t);}else{ System.out.printf("|-> tiempo: (%dms) - False \n",t); paso = false; }     //tiempo del proceso.1 min de mas de
+        if(c.getImagenesCargadas() == 100){ System.out.printf("|->Imagenes Cargadas: True \n"); }else{ System.out.printf("|->ImagenesCargadas: False \n"); paso = false; }  //Imagenes cargadas entre los hilos.
+        if(ci.getContenedorInicial().size() == 100 ){ System.out.printf("|->Cantidad correcta en el contenedor : True \n");}else{System.out.printf("|->Cantidad correcta en el contenedor : False \n"); paso = false;}  //si existen 100 imagenes cargadas en el contendor
+        if( imagenes_sin_repetir(ci) == 100 ){ System.out.printf("|->No se repiten los nombres: true \n"); }else{System.out.printf("|->No se repiten los nombres: False \n");paso = false; } //si los nombre no se repiten
+        if(paso){ System.out.printf("--> TRUE <-- Paso el Test de la carga \n"); }else{ System.out.printf("--> FALSE <-- NO paso el Test de la carga\n");paso = false;}
+        System.out.println("------------------------------------");
+    }
+
+    /**
+     * Testeo del proceso 2, Mejorar iluminacion a imagenes de un contenedor incial.
+     */
+    public void test_Mejorar(){
+
+    }
+
+    /**
+     * Testeo del proceso 3, Ajustar tamaño de imagenes de un contenedor incial.
+     */
+    public void test_Ajustar(){
+
+    }
+
+    /**
+     * Testeo del proceso 4, CargarCopia de las imagenes desde el contenedor inicial, al contenedor final.
+     */
+    public void test_CargarCopia(){
+        System.out.println("------------------------------------");
+    }
+    //-------------------------------------------------------------------------------------------------------------------------
+    //metodos comunes de uso.
+    /**
+     *
+     * @param ci
+     * @return numero de imagenes que estan una sola vez.
+     */
+    public int imagenes_sin_repetir(ContenedorInicial ci){
+        int imagenSinRepetir = 0;
+        for(Imagen aux1 : ci.getContenedorInicial()){
+            for(Imagen aux2 : ci.getContenedorInicial()){
+                if( aux1.getNombre().equals(aux2.getNombre())){  //true: son iguales los nombres
+                    imagenSinRepetir ++;
+                }
+            }
         }
-        System.out.printf("Tiempo de test_carga: %d" , t-1);
+        return imagenSinRepetir;
     }
 }
