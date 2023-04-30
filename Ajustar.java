01884851e7bd34960a2f66d583f613f8c46ce2fd
Efrain Veliz <efrainveliz4@gmail.com>
2023-04-30 08:57:25 -0300

    version2.3_testCompletos

--- Ajustar.java
+++ Ajustar.java
@@ -1,21 +1,13 @@
 /**
  * Ajustar: Proceso encargado de ajustar el tamaño de la imagen de manera sincronizada
  * hilos que ejecutan: hilo 6/7/8
-<<<<<<< HEAD
  * Acciones que realiza: -agarro una imagen random y pregunto si isTamanioOriginal es false y si esa imagen random fue ajustada de lo contrario la ajusto.
-=======
- * Acciones que realiza: -agarro una imagen random y pregunto si listo es false y si esa imagen random fue ajustada de lo contrario la ajusto.
->>>>>>> 24be326812d4de6bdaea534223a5b72898eccc15
  */
 public class Ajustar implements Runnable{
     private long tiempo;
     private boolean listo;
     private int imagenesAjustadas;
-<<<<<<< HEAD
     protected ContenedorInicial ci;
-=======
-    protected ContenedorInicial c;
->>>>>>> 24be326812d4de6bdaea534223a5b72898eccc15
 
     public Ajustar(long tiempo,ContenedorInicial ci,int cantidad_de_hilos) {
         this.tiempo = ( tiempo*cantidad_de_hilos )/(100 * 1); //(tiempo * hilos )/( 100 * actividades_sobre_cada_imagen )
@@ -26,30 +18,7 @@ public class Ajustar implements Runnable{
 
     @Override
     public void run() {
-        Random rand = new Random();
-        int indiceImagenaleatoria = rand.nextInt(c.lenght);// de forma aleatoria elijo un indice
-        String imagenAleatoria = c[indiceImagenaleatoria];  // busco la imagen en ese indice 
-        
-             while(listo == false && //que esa imagen no este ajustada ){
-                  ajustar(5,2); 
-                 
-             }
-    }
-    
-    
-    public void setListo(int i){
-        if(i == 100){
-            listo = true;
-        }
-    }
-    
-    public void ajustar(int l, int a)
-    {
-         imagenesAjustadas ++;
-         System.out.println("Imagen Ajustada: " + s); //dudas
-         c.ajuste(l,a); // esta en contenedor inicial no se si se implementa ahi o que
-         
-         setListo(imagenesAjustadas); //me fijo si son 100 o que
+
     }
 /*
     @Override
