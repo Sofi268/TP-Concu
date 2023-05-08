39851c1b9949be1bbc12039963bca81c8b46f0e9
Efrain Veliz <efrainveliz4@gmail.com>
2023-05-08 17:53:12 -0300

    version3.1_listo

--- Estadistico.java
+++ Estadistico.java
@@ -1,3 +1,12 @@
+/**
+ * Lleva la cuenta de cuantas imagenes se encuentran en cada proceso.
+ */
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Lleva la cuenta de cuantas imagenes se encuentran en cada proceso.
@@ -5,24 +14,30 @@
 public class Estadistico implements Runnable{
     private boolean listo;
     private Thread hilos[] = new Thread[10];
-    private int cantidadDeMuestras;
-    private int imagenesCargadas, imagenesMejoradas1,imagenesMejoradas2,imagenesMejoradas3, imagenesAjustadas,imagenesCopiadas;
-    public Estadistico(Thread hilos[]){
-        listo = false;
-        for(int i=0;i<10;i++){
-            this.hilos[i] = hilos[i];
+    private Cargar carga;
+    private Mejorar mejora;
+    private Ajustar ajuste;
+    private CargarCopia cargaCopia;
+    private int imagenesCargadas, imagenesMejoradas, imagenesAjustadas,imagenesCopiadas,cantidadDeMuestras;
+
+    public Estadistico(Cargar carga, Mejorar mejora, Ajustar ajuste, CargarCopia cargaCopia, Thread hilos[]){
+        this.carga = carga;
+        this.mejora = mejora;
+        this.ajuste = ajuste;
+        this.cargaCopia = cargaCopia;
+        for(int i=0;i<10;i++) {
+            this.hilos[i] = hilos[i+1];
         }
-        imagenesCargadas = hilos[1].getImagenesCargadas()+hilos[2].getImagenesCargadas();
-        imagenesMejoradas1 =hilos[3].getImagenesMejoradas();
-        imagenesMejoradas2 =hilos[4].getImagenesMejoradas();
-        imagenesMejoradas3 =hilos[5].getImagenesMejoradas();
-        imagenesAjustadas = hilos[6].getImagenesAjustadas()+hilos[7].getImagenesAjustadas()+hilos[8].getImagenesAjustadas();
-        imagenesCopiadas = hilos[9].getImagenesCopiadas()+hilos[10].getImagenesCopiadas();
+        listo = false;
         cantidadDeMuestras = 0;
+        imagenesCargadas=0;
+        imagenesMejoradas=0;
+        imagenesAjustadas=0;
+        imagenesCopiadas=0;
     }
     @Override
     public void run() {
-        try(FileWriter file = new FileWriter("\\log.txt");
+        try(FileWriter file = new FileWriter("D:\\UNC-Ing.Compu\\4to año\\Programacion concurrente\\TP1\\Trabajo_Practicos01\\src\\log.txt");
             PrintWriter pw = new PrintWriter(file); ) {
             pw.printf("************** Inicio del registro: %s ********* ", new Date());
             while (!listo) {
@@ -31,23 +46,31 @@ public class Estadistico implements Runnable{
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+                actualizarDatos();
                 imprimir(pw);
                 cantidadDeMuestras ++;
                 if (imagenesCopiadas == 100) {
                     listo = true;
                 }
             }
+            actualizarDatos();
+            imprimir(pw);
         }catch (IOException e){}
+    }
+    public void actualizarDatos(){
+        imagenesCargadas = carga.getImagenesCargadas();
+        imagenesMejoradas = mejora.getImagenesMejoradas();
+        imagenesAjustadas = ajuste.getImagenesAjustadas();
+        imagenesCopiadas = cargaCopia.getImagenesCopiadas();
     }
     public void imprimir(PrintWriter pw){
         pw.printf("------------------------------------------------------------------------\n");
-        pw.printf("P1-Cantidad de imagenes insertadas en el contenedor: %s\n",imagenesCargadas);
-        pw.printf("P2-Cantidad de imagenes mejoradas completamente por hilo: HILO_3:(%s) - HILO_4:(%s)  - HILO_5:(%s)  \n",imagenesMejoradas1,imagenesMejoradas2,imagenesMejoradas3);
+        pw.printf("P1-Cantidad de imagenes insertadas en el contenedor incial: %s\n",imagenesCargadas);
+        pw.printf("P2-Cantidad de imagenes mejoradas completamente: %s\n",imagenesMejoradas);
         pw.printf("P3-cantidad de imagenes ajustadas: %s\n",imagenesAjustadas);
-        pw.printf("P4-Cantidad de imagenes insertadas en el contenedor: %s\n",imagenesCopiadas);
+        pw.printf("P4-Cantidad de imagenes copiadas al contenedor final: %s\n",imagenesCopiadas);
         for(int i=0;i<10;i++){
             pw.printf("Estado %s : %s \n",hilos[i].getName(),hilos[i].getState());
         }
     }
-    //         System.out.printf("Cantidad de muestras: %d ",cantidadDeMuestras);
 }
--- Main.java
+++ Main.java
@@ -13,43 +13,44 @@ public class Main {
         Testeo prueba = new Testeo(tiempo);  //para comenzar con las pruebas.
         //DES-COMENTAR METODO PARA PROBAR PROCESO ELEGIDO. parametro: cantidad de hilos que deseo que carguen las imagenes.
         //for(int i=0;i<20;i++){  //para repetir 20 veces las pruebas.
-        prueba.test_Cargar(2);
-        prueba.test_Mejorar(3);
-        prueba.test_Ajustar(3);
-        prueba.test_CargarCopia(2);    //}
+        //prueba.test_Cargar(2);
+        //prueba.test_Mejorar(3);
+        //prueba.test_Ajustar(3);
+        //prueba.test_CargarCopia(2);    //}
 
-        tiempo = 5 * 1000; //debido a la espera de que cada imagen complete todos los procesos.
         ContenedorInicial ci = new ContenedorInicial();  //donde se cargaran las imagenes.
         ContenedorFinal cf = new ContenedorFinal();
         //creacion de procesos.
-        Estadistico log = new Estadistico();
         Cargar carga = new Cargar(tiempo,ci,2);    //Proceso 1:cargar las imagenes.
         Mejorar mejora = new Mejorar(tiempo,ci,3); //Proceso 2:Mejorar iluminacion de las imagenes.
         Ajustar ajuste = new Ajustar(tiempo,ci,3); //Proceso 3:Ajustar el tamaño de las imagenes.
         CargarCopia cargaCopia = new CargarCopia(tiempo,ci,cf,2); //Proceso 4:CargarCopia de las imagenes.
 
         //Creacion de Hilos. nota: modificar parametros a demanda.
-        Thread hilo0 = new Thread();  hilos[0]=hilo0 ; //Estadistico.
-        Thread hilo1 = new Thread(carga);  hilos[1]=hilo1; //P1:Carga.
-        Thread hilo2 = new Thread(carga);  hilos[2]=hilo2; //P1:Carga.
-        Thread hilo3 = new Thread(mejora);  hilos[3]=hilo3; //P2:Mejora de iluminacion.
-        Thread hilo4 = new Thread(mejora);  hilos[4]=hilo4; //P2:Mejora de iluminacion.
-        Thread hilo5 = new Thread(mejora);  hilos[5]=hilo5; //P2:Mejora de iluminacion.
-        Thread hilo6 = new Thread(ajuste);  hilos[6]=hilo6; //P3:Ajuste de tamanio.
-        Thread hilo7 = new Thread(ajuste);  hilos[7]=hilo7; //P3:Ajuste de tamanio.
-        Thread hilo8 = new Thread(ajuste);  hilos[8]=hilo8; //P3:Ajuste de tamanio.
-        Thread hilo9 = new Thread(cargaCopia);  hilos[9]=hilo9;   //P4:Copiar.
-        Thread hilo10 = new Thread(cargaCopia); hilos[10]=hilo10; //P4:Copiar.
+        Thread hilo1 = new Thread(carga); hilo1.setPriority(10); hilos[1]=hilo1; //P1:Carga.
+        Thread hilo2 = new Thread(carga); hilo2.setPriority(10); hilos[2]=hilo2; //P1:Carga.
+        Thread hilo3 = new Thread(mejora); hilo3.setPriority(8); hilos[3]=hilo3; //P2:Mejora de iluminacion.
+        Thread hilo4 = new Thread(mejora); hilo4.setPriority(8); hilos[4]=hilo4; //P2:Mejora de iluminacion.
+        Thread hilo5 = new Thread(mejora); hilo5.setPriority(8); hilos[5]=hilo5; //P2:Mejora de iluminacion.
+        Thread hilo6 = new Thread(ajuste); hilo6.setPriority(5); hilos[6]=hilo6; //P3:Ajuste de tamanio.
+        Thread hilo7 = new Thread(ajuste); hilo7.setPriority(5); hilos[7]=hilo7; //P3:Ajuste de tamanio.
+        Thread hilo8 = new Thread(ajuste); hilo8.setPriority(5); hilos[8]=hilo8; //P3:Ajuste de tamanio.
+        Thread hilo9 = new Thread(cargaCopia); hilo9.setPriority(3); hilos[9]=hilo9;   //P4:Copiar.
+        Thread hilo10 = new Thread(cargaCopia); hilo10.setPriority(3); hilos[10]=hilo10; //P4:Copiar.
+
+        Estadistico log = new Estadistico(carga,mejora,ajuste,cargaCopia,hilos);
+        Thread hilo0 = new Thread(log); hilo0.setPriority(1); hilos[0]=hilo0 ; //Estadistico.
 
         long startTimeMain = System.currentTimeMillis();
-        for (int i=0;i<10;i++){ //iniciamos los hilos.
+        for (int i=0;i<=10;i++){ //iniciamos los hilos.
             hilos[i].start();
         }
 
         try{  //Detenemos los hilos.
-            for (int i=0;i<10;i++){ //iniciamos los hilos.
+            for (int i=1;i<=10;i++){ //iniciamos los hilos.
                 hilos[i].join();
             }
+            hilo0.join(); //hilo del estadistico
         }catch(InterruptedException e){ throw new RuntimeException(e);}
         long endTimeMain = System.currentTimeMillis();
         long t = endTimeMain - startTimeMain; //tiempo en miliSegundos.
